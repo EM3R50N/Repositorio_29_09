e3346c9dfcf8cc0403d9fa92c2eabf7e48455672
Emerson <emersoncuentaingreso@gmail.com>
2022-09-29 16:18:22 -0500

    Untitledcccc.cpp

---
+++ Untitledcccc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void llenar();
+void amm(int X[], int M[], int);
+void mostrar(int);
+
+int n, X[50], N[50], M[50];
+	
+int main(){
+	llenar();
+	amm(X, M, n);
+	mostrar(n);
+}
+
+void llenar(){
+	cout<<"Tamaño: ";cin>>n;
+	for(int i=0; i<n; i++){
+		cout<<"\nNumero: ";cin>>X[i];
+		cout<<"Numero: ";cin>>M[i];
+	}
+}
+
+void amm(int X[], int M[], int n){
+	int N[50];
+	for(int i=0; i<n; i++){
+		N[i]=X[i]-M[i];
+	}
+}
+
+void mostrar(int n){
+	for(int i=0; i<n; i++){
+		cout<<N[i];
+	}
+}
