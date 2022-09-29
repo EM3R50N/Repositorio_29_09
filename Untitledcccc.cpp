b98eeda6300d4218b1b75d9edc791090b77dbba6
EM3R50N <114238995+EM3R50N@users.noreply.github.com>
2022-09-29 17:38:23 -0500

    Update Untitledcccc.cpp

--- Untitledcccc.cpp
+++ Untitledcccc.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 using namespace std;
-void llenar();
-void amm(int X[], int M[], int);
+void llenar(int& X[], int& M[], int);
+void amm(int X[], int M[], int& N[], int);
 void mostrar(int);
-
-int n, X[50], N[50], M[50];
 	
 int main(){
-	llenar();
-	amm(X, M, n);
+	int n, X[50], N[50], M[50];
+	llenar(X, M, n);
+	amm(X, M, N, n);
 	mostrar(n);
 }
 
-void llenar(){
-	cout<<"Tamaño: ";cin>>n;
+void llenar(int& X[], int% M[], int n){
+	cout<<"TamaÃ±o: ";cin>>n;
 	for(int i=0; i<n; i++){
 		cout<<"\nNumero 1: ";cin>>X[i];
 		cout<<"Numero 2: ";cin>>M[i];
 	}
 }
 
-void amm(int X[], int M[], int n){
-	int N[50];
+void amm(int X[], int M[], int N[], int n){
 	for(int i=0; i<n; i++){
 		N[i]=X[i]-M[i];
 	}
